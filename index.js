282d7232b127b97d277f1c9a96ebb76630c48a7a
zecarlos94 <zecarlosbg94@gmail.com>
2019-04-01 09:41:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 const fs = require("fs");
-const inquirer = require('inquirer');
 const config = require('./config');
-
-const questions = [
-    {
-        type: 'input',
-        name: 'componentName',
-        message: "Insert Component Name",
-        validate: function (value) {
-            var pass = value.match(/[A-Za-z]+/i);
-            if (pass) {
-                return true;
-            }
-            return 'Please enter a valid name';
-        }
-    }
-];
 
 function writeTemplates(componentName, extension, content) {
     fs.writeFile(componentName + ".component." + extension, content, function (err, data) {
@@ -23,14 +7,13 @@ function writeTemplates(componentName, extension, content) {
     });
 };
 
-(function generateTemplates() {
-    let componentName = '';
-    inquirer.prompt(questions).then(answers => {
-        componentName = answers['componentName'];
-
+function generateTemplates(componentName) {
+    if(componentName && componentName.match(/[A-Za-z]+/i)) {
         config.templates.forEach(element => {
             const currentTemplate = require(config.templatesFolderPath+"/"+element.templateName.split('.js')[0]);
-            writeTemplates(componentName, element.extension, currentTemplate(componentName));
+            writeTemplates(componentName.toLowerCase(), element.extension, currentTemplate(componentName.toLowerCase()));
         });
-    });
-})();
+    }
+};
+
+module.exports = (componentName) => generateTemplates(componentName);
